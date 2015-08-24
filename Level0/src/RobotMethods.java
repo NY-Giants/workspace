c0ab0028c293223ed1819a8eff3d6166bfb73fcb
NY-Giants <Guest@wts9.attlocal.net>
2015-08-23 17:34:05 -0700

    java

---
+++ Level0/src/RobotMethods.java
@@ -0,0 +1,70 @@
+import org.jointheleague.graphical.robot.Robot;
+
+
+public class RobotMethods {
+	public static void main(String[] args) {
+		Robot C = new Robot ();
+		
+	//	drawsquare(bob,179);
+//	drawtriangle( C,120);
+	}
+
+//static	void drawsquare(Robot R, int size){
+		
+		
+//		R.setSpeed(10);	
+//R.penDown();
+//for( int i=0; i <=20; i++) {
+//	R.move(100);
+	//R.turn(size);
+		
+		
+	
+	
+	
+//	}
+//}
+	//static void drawtriangle(Robot C, int length )
+	//{
+//	C.setSpeed(10);
+//	for (int i = 0; i <= 3; i++ ) {
+//C.penDown ();
+//C.move(length);
+//C.turn(120);
+	
+	
+	
+	
+	
+//	}	
+//	}	
+static void drawshape(Robot D, int length, int sides) {	
+D.setSpeed(10);
+for(int i = 0; i<side; i++) {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}	
+}
